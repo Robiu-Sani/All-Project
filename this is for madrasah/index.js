bc77fa535f95ed290e12db609c9fd5a975e00ce8
Robius Sani <148758251+Robiu-Sani@users.noreply.github.com>
2023-11-08 09:20:06 -0800

    html and css design

---
+++ this is for madrasah/index.js
@@ -0,0 +1,63 @@
+const class1 = document.querySelector(".slider-pic1"),
+class2 = document.querySelector(".slider-pic2"),
+class3 = document.querySelector(".slider-pic3"),
+class4 = document.querySelector(".slider-pic4"),
+leftBtn = document.querySelector(".leftbtn"),
+rightBtn = document.querySelector(".rightbtn");
+
+const slide = [class1,class2,class3,class4];
+
+slide[0].classList.add("active");
+
+var x = 0;
+
+function leftSlide(){
+
+setInterval(function(){
+   if(x == slide.length-1){
+      x = 0;
+      slide[0].classList.add("active");
+      slide[3].classList.remove("active");
+   }else{
+      x++;
+      a=slide[x];
+      if(a==slide[1]){
+         slide[1].classList.add("active");
+         slide[0].classList.remove("active");
+      }else if(a==slide[2]){
+         slide[2].classList.add("active");
+         slide[1].classList.remove("active");
+      }else{
+         slide[3].classList.add("active");
+         slide[2].classList.remove("active");
+      }
+   }   
+},5000);
+}
+leftSlide();
+
+
+leftBtn.addEventListener("click",function(){
+   leftSlide();
+});
+
+rightBtn.addEventListener("click",function(){
+   if(x == slide.length-1){
+      x = 0;
+     slide[0].classList.add("active");
+     slide[3].classList.remove("active");
+  }else{
+      x++;
+      a=slide[x];
+      if(a==slide[1]){
+         slide[1].classList.add("active");
+         slide[0].classList.remove("active");
+      }else if(a==slide[2]){
+         slide[2].classList.add("active");
+         slide[1].classList.remove("active");
+      }else{
+         slide[3].classList.add("active");
+         slide[2].classList.remove("active");
+      }
+  }  
+});
